3599b59d07aba969fecc7d5096efd112d765c517
GADUPOOTI <50094813+GADUPOOTI@users.noreply.github.com>
2019-05-01 16:48:23 +0530

    Add files via upload

---
+++ DivTwo.java
@@ -0,0 +1,9 @@
+class DivTwo
+{
+public static void main(String args[]) 
+   {
+        int num1 =15, num2 =3, sum;
+      sum =num1 / num2;
+ System.out.println("Sum of these numbers: "+sum);
+   }
+}
