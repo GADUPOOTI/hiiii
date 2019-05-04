0f1091a8ec9c395630f584a8692f7a4abe64cdb7
GADUPOOTI <50094813+GADUPOOTI@users.noreply.github.com>
2019-05-04 22:00:53 +0530

    Delete DivTwo.java

--- DivTwo.java
+++
@@ -1,9 +0,0 @@
-class DivTwo
-{
-public static void main(String args[]) 
-   {
-        int num1 =15, num2 =3, sum;
-      sum =num1 / num2;
- System.out.println("Sum of these numbers: "+sum);
-   }
-}
