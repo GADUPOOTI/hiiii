5bf51892ce3234820e3b4093f9f18aea422f19ad
GADUPOOTI <50094813+GADUPOOTI@users.noreply.github.com>
2019-05-04 22:01:32 +0530

    Delete SubTwo.java

--- SubTwo.java
+++
@@ -1,9 +0,0 @@
-class SubTwo
-{
-public static void main(String args[]) 
-   {
-        int num1=15, num2=5, sum;
-      sum =num1 - num2;
- System.out.println("Sum of these numbers: "+sum);
-   }
-}
